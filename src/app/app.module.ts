8bb13c114fccf9ba3f9c25e12c990c792ce2010d
ealvess <everton10alves@gmail.com>
2018-03-28 13:57:36 -0400

    crud de contatos completo

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { AngularFireModule }  from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ContactProvider } from '../providers/contact/contact';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyDMfQ9cOWZgPYegizDCZd4CbKU3Nv9PY6I",
+      authDomain: "tutorial-d0829.firebaseapp.com",
+      databaseURL: "https://tutorial-d0829.firebaseio.com",
+      projectId: "tutorial-d0829",
+      storageBucket: "tutorial-d0829.appspot.com",
+      messagingSenderId: "290899237291"
+    }),
+    AngularFireDatabaseModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ContactProvider
+  ]
+})
+export class AppModule {}
